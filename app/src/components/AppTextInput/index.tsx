9725eb32b5f71a94fd5e3e6fc61bdbca409ee01a
Alan-Graton <graton.alan@gmail.com>
2023-11-04 15:53:22 -0300

    wip: init components and app theme developement

---
+++ app/src/components/AppTextInput/index.tsx
@@ -0,0 +1,3 @@
+export function AppTextInput() {
+  return <></>;
+}
--- app/src/theme/index.ts
+++ app/src/theme/index.ts
@@ -1,5 +1,23 @@
 export const THEME = {
-  COLORS: {},
+  COLORS: {
+    RED_300: "#BF3B44",
+    RED_200: "#F3BABD",
+    RED_100: "#F4E6E7",
+
+    GREEN_300: "#639339",
+    GREEN_200: "#CBE4B4",
+    GREEN_100: "#E5F0DB",
+
+    GRAY_700: "#1B1D1E",
+    GRAY_600: "#333638",
+    GRAY_500: "#5C6265",
+    GRAY_400: "#B9BBBC",
+    GRAY_300: "#DDDEDF",
+    GRAY_200: "#EFF0F0",
+    GRAY_100: "#FAFAFA",
+
+    WHITE: "#FFFFFF",
+  },
   FONT_SIZE: {},
   FONT_FAMILY: {},
 };
