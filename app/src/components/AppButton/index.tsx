5ad836cabd670718de98b6bda618a58db8f1f434
Alan-Graton <graton.alan@gmail.com>
2023-11-04 20:47:40 -0300

    wip: created a skeleton for `AppButton` component

--- app/src/components/AppButton/index.tsx
+++ app/src/components/AppButton/index.tsx
@@ -1,3 +1,13 @@
-export function AppButton() {
-  return <></>;
+import * as S from "./styles";
+
+type Props = {
+  title: string;
+};
+
+export function AppButton({ title, ...rest }: Props) {
+  return (
+    <S.Button {...rest}>
+      <S.Title>{title}</S.Title>
+    </S.Button>
+  );
 }
---
+++ app/src/components/AppButton/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components/native';
+
+export const Button = styled.TouchableOpacity``;
+
+export const Title = styled.Text``;
