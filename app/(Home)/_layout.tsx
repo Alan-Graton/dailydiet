7b3da7ac8f3014f5caf943625a9bd5870d1e40bb
Alan-Graton <graton.alan@gmail.com>
2023-11-04 20:09:12 -0300

    wip: styling Home header

--- app/(Home)/_layout.tsx
+++ app/(Home)/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router/stack";
 
+import UserProfilePic from "../src/assets/UserProfilePic.png";
+import AppLogo from "../src/assets/Logo.png";
+
+import { Logo, ProfilePic } from "./styles";
+
 export default function HomeLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: true,
+        headerShadowVisible: false,
+        title: "",
+        headerLeft: () => <Logo source={AppLogo} />,
+        headerRight: () => <ProfilePic source={UserProfilePic} />,
+      }}
+    />
+  );
 }
