fef2467b536b9bd2aed1ea5134603c19088eedeb
Alan-Graton <graton.alan@gmail.com>
2023-11-04 23:21:25 -0300

    wip: create temp constants for `DietFeddback` screen

---
+++ app/src/constants/ErrorDietFeedback.ts
@@ -0,0 +1,7 @@
+const ErrorTitle = "Que Pena!";
+
+// TODO: Text(dentro da dieta) need to be bold
+const ErrorSubTitle =
+  "Você saiu da dieta dessa vez, mas continue se esforçando e não desista!";
+
+export { ErrorTitle, ErrorSubTitle };
