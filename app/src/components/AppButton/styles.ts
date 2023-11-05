7928e9bd4d26296f748b1aa516ee1002948372cc
Alan-Graton <graton.alan@gmail.com>
2023-11-04 22:59:57 -0300

    wip: apply styles to `AppButton` component

--- app/src/components/AppButton/styles.ts
+++ app/src/components/AppButton/styles.ts
@@ -1,5 +1,23 @@
-import styled from 'styled-components/native';
+import styled, { css } from "styled-components/native";
 
-export const Button = styled.TouchableOpacity``;
+export const Button = styled.TouchableOpacity`
+  padding: 10px;
 
-export const Title = styled.Text``;
+  width: 280px;
+  height: 65px;
+
+  align-items: center;
+  justify-content: center;
+
+  border-radius: 6px;
+
+  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
+`;
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.SM}px;
+    font-weight: bold;
+    color: ${theme.COLORS.WHITE};
+  `}
+`;
