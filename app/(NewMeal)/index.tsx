6623e31be5fe848135fff8f5164160ea3f21edc8
Alan-Graton <graton.alan@gmail.com>
2023-11-18 12:05:40 -0300

    fix: major code enhancement and UI adjusts

--- app/(NewMeal)/index.tsx
+++ app/(NewMeal)/index.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { View, Text } from "react-native";
 import { Stack } from "expo-router";
 
-import { SafeAreaView } from "react-native-safe-area-context";
-
 // Application Components
 import { AppTextInput } from "@/components/AppTextInput";
 import { AppTextArea } from "@/components/AppTextArea";
@@ -20,61 +18,35 @@ export default function NewMeal() {
     <>
       <S.Container>
         <Stack.Screen options={{ header: () => <Header /> }} />
-        <S.Form
-          className="Form"
-          // style={{
-          //   alignItems: "center",
-          //   padding: 16,
-          //   gap: 30,
-          //   flex: 1,
-          //   width: "100%",
-          // }}
-        >
+        <S.Form>
           <View style={{ width: "100%" }}>
-            <Text style={{ fontWeight: "bold", alignSelf: "flex-start" }}>
-              Nome
-            </Text>
+            <S.TextInputLabel>Nome</S.TextInputLabel>
             <AppTextInput />
           </View>
           <View style={{ width: "100%" }}>
-            <Text style={{ fontWeight: "bold", alignSelf: "flex-start" }}>
-              Descrição
-            </Text>
+            <S.TextInputLabel>Descrição</S.TextInputLabel>
             <AppTextArea />
           </View>
-          <View className="DateTime" style={{ flexDirection: "row", gap: 15 }}>
-            <View style={{ flex: 1, gap: 10 }}>
+          <S.DateTimeContainer>
+            <S.DateTimeForm>
               <Text style={{ fontWeight: "bold" }}>Data</Text>
               <AppTextInput />
-            </View>
-            <View style={{ flex: 1, gap: 10 }}>
+            </S.DateTimeForm>
+            <S.DateTimeForm>
               <Text style={{ fontWeight: "bold" }}>Horário</Text>
               <AppTextInput />
-            </View>
-          </View>
-          <View className="DietStatus" style={{ marginBottom: "16%" }}>
-            <Text style={{ fontWeight: "bold" }}>Status</Text>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-between",
-                gap: 10,
-              }}
-            >
+            </S.DateTimeForm>
+          </S.DateTimeContainer>
+          <S.DietStatusContainer>
+            <Text style={{ fontWeight: "bold" }}>Está dentro da dieta?</Text>
+            <S.DietStatusForm>
               <AppSelect title="Sim" />
               <AppSelect title="Não" type="ERROR" />
-            </View>
-          </View>
-          <View
-            className="Footer"
-            style={{
-              width: "100%",
-              alignItems: "center",
-            }}
-          >
-            <AppButton title="Cadastrar" />
-          </View>
+            </S.DietStatusForm>
+          </S.DietStatusContainer>
+          <S.Footer>
+            <AppButton title="Cadastrar refeição" />
+          </S.Footer>
         </S.Form>
       </S.Container>
     </>
