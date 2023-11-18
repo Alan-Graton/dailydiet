006f7cbeaf8869494fd6241dc169546e0c724edc
Alan-Graton <graton.alan@gmail.com>
2023-11-18 13:00:04 -0300

    feat: add back function to Header button

--- app/(MealDetails)/components/Header/index.tsx
+++ app/(MealDetails)/components/Header/index.tsx
@@ -1,12 +1,16 @@
 import * as S from "./styles";
 
+import { useRouter } from "expo-router";
+
 // Application Components
 import { AppBackButtonIcon } from "@/components/AppBackButtonIcon";
 
 export function Header() {
+  const navigation = useRouter();
+
   return (
     <S.Container>
-      <AppBackButtonIcon />
+      <AppBackButtonIcon onPress={() => navigation.back()} />
       <S.Title>Refeição</S.Title>
     </S.Container>
   );
