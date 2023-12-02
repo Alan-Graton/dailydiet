95a85860818709c8ee7a8ca71172a648a8c0ea32
Alan Graton Lourenço de Brito <graton.alan@gmail.com>
2023-12-02 20:26:15 -0300

    wip: preparing api for `Statistics` screen

--- app/(Statistics)/index.tsx
+++ app/(Statistics)/index.tsx
@@ -1,17 +1,40 @@
+import React from "react";
+import { useIsFocused } from "@react-navigation/native";
+
 import { Stack } from "expo-router";
 
-// Screen Components
 import { Header } from "./components/Header";
 import { BestMealSequence } from "./components/BestMealSequence";
 import { MealCounter } from "./components/MealCounter";
 import { DietFeedbackCounter } from "./components/DietFeedbackCounter";
 
-// Styles
 import * as S from "./styles";
 import { useTheme } from "styled-components/native";
+import { getGeneralDetails } from "@/storage/getGeneralDetails";
 
 export default function Statistics() {
+  const isFocused = useIsFocused();
   const { COLORS } = useTheme();
+
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [statistics, setStatistics] = React.useState<any[]>([]);
+
+  React.useEffect(() => {
+    async function getStatistics() {
+      const response = await getGeneralDetails();
+
+      return response;
+    }
+
+    if (isFocused) {
+      setLoading(true);
+      const data = getStatistics();
+      if (!data) return;
+
+      setStatistics((prevState) => (prevState = data as any));
+      setLoading(false);
+    }
+  }, [isFocused]);
 
   return (
     <S.Container>
--- app/src/storage/getGeneralDetails.ts
+++ app/src/storage/getGeneralDetails.ts
@@ -1,6 +1,11 @@
+import { getAllMeals } from "./getAllMeals";
+
 export async function getGeneralDetails() {
   try {
     console.log("\n\n[getGeneralDetails] Running...");
+    const allMeals = await getAllMeals();
+
+    return allMeals;
   } catch (e) {
     console.error("\n\n[getGeneralDetails] Error: ", e);
   }
