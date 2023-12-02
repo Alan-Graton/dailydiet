47a80e3fcace3ffa29c97571ac99e256ccfed66d
Alan Graton Lourenço de Brito <graton.alan@gmail.com>
2023-12-02 20:25:25 -0300

    chore: types enhancement

--- app/src/storage/getAllMeals.ts
+++ app/src/storage/getAllMeals.ts
@@ -1,13 +1,14 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { StorageEntity } from "./config/StorageConfig";
+import { IMealList } from "@/interfaces";
 
-export async function getAllMeals(): Promise<any> {
+export async function getAllMeals(): Promise<IMealList[] | void> {
   try {
     const response = await AsyncStorage.getItem(StorageEntity);
 
     if (!response) return;
 
-    return JSON.parse(response);
+    return JSON.parse(response) as IMealList[];
   } catch (e) {
     console.error("\n\n[getAllMeals] Error: ", e);
   }
