96c0caa889c4cb2d36f352e6ce1c78a361167a13
Alan Graton Lourenço de Brito <graton.alan@gmail.com>
2023-12-02 20:12:46 -0300

    fix: major adjusts to storin/fetching data in AsyncStorage

--- app/src/storage/getAllMeals.ts
+++ app/src/storage/getAllMeals.ts
@@ -1,18 +1,13 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { StorageEntity } from "./config/StorageConfig";
-import { IMealList } from "@/interfaces";
 
-type Args = React.Dispatch<React.SetStateAction<IMealList[]>>;
-
-export async function getAllMeals(setMeals: Args) {
+export async function getAllMeals(): Promise<any> {
   try {
     const response = await AsyncStorage.getItem(StorageEntity);
 
-    console.log("\n\n[getAllMeals] Response: ", response);
+    if (!response) return;
 
-    const transformMealList = response !== null ? JSON.parse(response) : null;
-
-    setMeals((prevState) => [...prevState, transformMealList]);
+    return JSON.parse(response);
   } catch (e) {
     console.error("\n\n[getAllMeals] Error: ", e);
   }
--- app/src/storage/postMeal.ts
+++ app/src/storage/postMeal.ts
@@ -2,15 +2,19 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { IMealDTO } from "./config/MealDTO";
 import { StorageEntity } from "./config/StorageConfig";
+import { getAllMeals } from "./getAllMeals";
 
 type Args = IMealDTO;
 
 export async function postMeal(meal: Args): Promise<void> {
   try {
-    console.log("\n\nSaving Meal: ", meal);
+    const allMeals = await getAllMeals();
 
-    const body = JSON.stringify(meal);
-    AsyncStorage.setItem(StorageEntity, body);
+    const areAllMealsArray = Array.isArray(allMeals) && allMeals.push(meal);
+
+    const body = !areAllMealsArray ? [meal] : allMeals;
+
+    await AsyncStorage.setItem(StorageEntity, JSON.stringify(body));
   } catch (e) {
     console.error("\n\n[postMeal] Error: ", e);
   }
